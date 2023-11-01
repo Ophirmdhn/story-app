7888f422d544483e1a306d24da9ade77aba696d9
Ophirmdhn <ophiramadhan18@gmail.com>
2023-11-01 23:33:40 +0700

    Membuat halaman register

--- app/src/main/java/com/ophi/storyapp/ui/MainActivity.kt
+++ app/src/main/java/com/ophi/storyapp/ui/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.ophi.storyapp
+package com.ophi.storyapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ophi.storyapp.R
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
     }
 }
